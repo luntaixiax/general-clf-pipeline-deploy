c8f79cc98eb74b0dcce5705ccca00ab2233234da
luntaixiax <ailunqian124@gmail.com>
2024-03-07 23:35:18 -0500

    fix clickhouse version

--- build-images.sh
+++ build-images.sh
@@ -4,3 +4,11 @@
 cd fake-data-generator
 docker build -t luntaixia/general-clf-faker .
 docker push luntaixia/general-clf-faker
+
+# test image
+docker pull luntaixia/general-clf-faker
+docker run -it --rm \
+ -e SECRET_TOML_PATH=/app-mount/secrets.toml \
+ -v /home/luntaixia/Downloads/docker-volume-mapping/general_clf_pipeline_volume/app-mount:/app-mount \
+ luntaixia/general-clf-faker \
+ python -m src.dag_run test_run
